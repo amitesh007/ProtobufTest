77aae952879d589ab6a0a242e0f668a68d15953e
Amitesh Ranjan Srivastava <amitesh430@gmail.com>
2023-01-19 10:44:07 +0000

    Initial commit

---
+++ src/test/java/com/finastra/protobuf/ProtobufTestTest.java
@@ -0,0 +1,21 @@
+package com.finastra.protobuf;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class ProtobufTestTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
